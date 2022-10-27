31649fa3df09da18ad8bb84d24bfaf007eeaa109
Osamacapa <majinantonio16@gmail.com>
2022-10-27 07:38:02 -0500

    no validacion de correo

--- main.js
+++ main.js
@@ -6,18 +6,12 @@ const celular=document.querySelector('#celular')
 const parrafo=document.querySelector('.texto1')
 const parrafo2=document.querySelector('.texto2')
 
-const correo2=/([a-zA-Z0-9])+@(([a-zA-Z0-9])+.)+com/
-
 
 enviar.addEventListener('click',()=>{
     if(nombre.value==='' && apellido.value==='' && correo.value==='' && celular.value===''){
         parrafo.innerHTML=('Algo hace falta, complete todos los campos')
     }else{
         console.log('ok')
-    }
-
-    if(correo2 != correo.value){
-        parrafo2.innerHTML=('Correo electronico invalido')
     }
 })
 
